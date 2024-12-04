0217fe88c7cb844795d5c07ac79598b8a677cf1c
LibrazionPc <nwajeizion393@gmail.com>
2024-12-04 23:07:58 +0100

    Fisrt push

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,16 @@
+// 0-redis_client.js
+
+import redis from 'redis';
+
+// Create a new Redis client
+const client = redis.createClient();
+
+// Handle successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Handle connection errors
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,34 @@
+// 1-redis_op.js
+
+import redis from 'redis';
+
+
+const client = redis.createClient();
+
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to set a new school in Redis
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, value) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(value);
+    }
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
